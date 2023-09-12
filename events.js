33db6782053722d5d5494b4907d564136956082a
iHamza7 <khanazam3324@gmail.com>
2023-09-12 23:47:52 +0500

    changed

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ MyEmitter.on("NewSale", () => {
   console.log("this is new sale");
 });
 MyEmitter.on("NewSale", () => {
-  console.log("Customer Name: Hamza khan");
+  console.log("Customer Name: Ha khan");
 });
 
 MyEmitter.emit("NewSale");
