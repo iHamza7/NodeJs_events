fdbdf8c442c75e608ee0ddd3fdfddad77a1abeaf
iHamza7 <khanazam3324@gmail.com>
2023-09-12 23:48:02 +0500

    lib changed

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ MyEmitter.on("NewSale", () => {
   console.log("this is new sale");
 });
 MyEmitter.on("NewSale", () => {
-  console.log("Customer Name: Ha khan");
+  console.log("Customer Name: Ham khan");
 });
 
 MyEmitter.emit("NewSale");
