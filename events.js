f30586c82b68b6286704ff19cf3d74c8e407931f
iHamza7 <khanazam3324@gmail.com>
2023-09-12 23:49:18 +0500

    changed

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ MyEmitter.on("NewSale", () => {
   console.log("this is new sale");
 });
 MyEmitter.on("NewSale", () => {
-  console.log("Customer Name: hamz khan");
+  console.log("Customer Name: hamza khan");
 });
 
 MyEmitter.emit("NewSale");
